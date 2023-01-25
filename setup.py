0624389caf25321ca2dee16cf4eea074eb01d46d
ilfrich <das-peter@gmx.de>
2023-01-25 13:09:32 +1100

    incremented version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name="frappyflaskimpex",
-      version="1.0.0",
+      version="1.0.1",
       description="Flask Endpoints for Database Import and Export",
       long_description=long_description,
       long_description_content_type="text/markdown",
